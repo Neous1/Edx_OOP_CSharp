4d33b12696c0bf39fad3c9b10281c6989fa7d959
Neous1 <Neous1@users.noreply.github.com>
2018-02-16 18:36:36 -0500

    create DrinksMachine Class

--- CreatingClasses/CreatingClasses/DrinksMachine.cs
+++ CreatingClasses/CreatingClasses/DrinksMachine.cs
@@ -8,5 +8,20 @@ namespace CreatingClasses
 {
     class DrinksMachine
     {
+        public string _location;
+        public string _make;
+        public string _model;
+
+        // The following stattements declasre plucblck mehtods 
+        public void MakeCappicino()
+        {
+            //Method logi goes here
+        }
+
+        public void MakeEsspressso()
+        {
+            
+            //Method logic goe here
+        }
     }
 }
