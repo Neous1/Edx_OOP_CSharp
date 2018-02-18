e6678f03f27f8f605b39dac99a63321da3a37233
Neous1 <Neous1@users.noreply.github.com>
2018-02-18 16:05:22 -0500

    instantiate the class car

--- Mod1_Lab1/Mod1_Lab1/Program.cs
+++ Mod1_Lab1/Mod1_Lab1/Program.cs
@@ -10,6 +10,16 @@ namespace Mod1_Lab1
     {
         static void Main(string[] args)
         {
+            Car Car1 = new Car();
+
+            //call members of Car1
+
+            Car1.Color = "White";
+            Car1.Year = 2010;
+            Car1.Mileage = 11000;
+
+            Console.WriteLine($"This car is painted {Car1.Color}, was built in {Car1.Year}, and has {Car1.Mileage} miles on it.");
+
         }
     }
     // Declaring the Car() Class
